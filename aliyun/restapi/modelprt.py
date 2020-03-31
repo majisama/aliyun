c9870c9fd7ca29b1e1fbf24ff5d027711a1982ef
majisama <62916251+majisama@users.noreply.github.com>
2020-03-31 21:20:10 +0800

    Update modelprt.py

--- aliyun/restapi/modelprt.py
+++ aliyun/restapi/modelprt.py
@@ -12,12 +12,10 @@ class prd:
         global model  
         keras.backend.clear_session() 
         model = load_model(dirname+"/model.h5")
-        
     def prdt(self,data):
         samehours=np.log10(data[:144].reshape(6,24))
         prdts=np.array([])
         for i in range(0,3):
-            #没办法了,重复使用会报错
             pd=model.predict([[samehours]])
             samehours=np.delete(samehours,0,axis=0) 
             samehours=np.append(samehours,pd[0][0])
